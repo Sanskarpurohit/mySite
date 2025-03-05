487c785f6f9046500509362aa125e2db3cdc9138
Purohit <2358388@cognizant.com>
2025-03-05 13:02:17 +0530

    add tools folder

---
+++ blocks/game/game.js
@@ -0,0 +1,18 @@
+let heading=document.querySelector(".game h1");
+console.log(heading);
+if(heading){
+    heading.classList.add("title");   
+    console.log(heading); 
+}
+let para=document.querySelector(".game p");
+console.log(para);
+if(para){
+    para.classList.add("paragrapgh");   
+    console.log(para); 
+}
+let picture=document.querySelector(".game picture");
+console.log(picture);
+if(picture){
+    picture.classList.add("arthur-img");   
+    console.log(picture); 
+}
