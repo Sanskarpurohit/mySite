5d06a73ff8dd9707eba54d28d0ae19e48d4c88ef
Sanskarpurohit <Sanskar.Purohit@cognizant.com>
2025-05-09 11:11:22 +0530

    added homepage block

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,97 +1,95 @@
-import { fetchPlaceholders,getMetadata } from '../../scripts/aem.js';
-const placeholders = await fetchPlaceholders(getMetadata("locale"));
+document.addEventListener("DOMContentLoaded", function() {
 
-const { allCountries,abbreviation,africa,america,asia,australia,capital,continent,countries,europe,sNo} = placeholders;
+    const carousel = document.querySelector('.carousel'); // Select the carousel container
 
+    const slides = carousel.querySelectorAll('.carousel > div:nth-child(2)'); // Get all slide items
 
-async function createTableHeader(table){
-    let tr=document.createElement("tr");
-    let sno=document.createElement("th");sno.appendChild(document.createTextNode(sNo));
-    let conuntry=document.createElement("th");conuntry.appendChild(document.createTextNode(countries));
-    let continenth=document.createElement("th");continenth.appendChild(document.createTextNode(continent));
-    let capitalh=document.createElement("th");capitalh.appendChild(document.createTextNode(capital));
-    let abbr=document.createElement("th");abbr.appendChild(document.createTextNode(abbreviation));
-    tr.append(sno);tr.append(conuntry);tr.append(capitalh);tr.append(continenth);tr.append(abbr);
-    table.append(tr);
-}
-async function createTableRow(table,row,i){
-    let tr=document.createElement("tr");
-    let sno=document.createElement("td");sno.appendChild(document.createTextNode(i));
-    let conuntry=document.createElement("td");conuntry.appendChild(document.createTextNode(row.Country));
-    let continent=document.createElement("td");continent.appendChild(document.createTextNode(row.Capital));
-    let capital=document.createElement("td");capital.appendChild(document.createTextNode(row.Continent));
-    let abbr=document.createElement("td");abbr.appendChild(document.createTextNode(row.Abbreviation));
-    tr.append(sno);tr.append(conuntry);tr.append(continent);tr.append(capital);tr.append(abbr);
-    table.append(tr);
-}
+    // Get the Next and Previous buttons (First and Last divs)
 
-async function createSelectMap(jsonURL){
-    const optionsMap=new Map();
-    const { pathname } = new URL(jsonURL);
+    const firstNavButton = carousel.querySelector('div > div:first-child p'); // First button (Next)
 
-    const resp = await fetch(pathname);
-    optionsMap.set("all",allCountries);optionsMap.set("asia",asia);optionsMap.set("europe",europe);optionsMap.set("africa",africa);optionsMap.set("america",america);optionsMap.set("australia",australia);
-    const select=document.createElement('select');
-    select.id = "region";
-    select.name="region";
-    optionsMap.forEach((val,key) => {
-        const option = document.createElement('option');
-        option.textContent = val;
-        option.value = key;
-        select.append(option);
-      });
-     
-     const div=document.createElement('div'); 
-     div.classList.add("region-select");
-     div.append(select);
-    return div;
-}
-async function createTable(jsonURL,val) {
+    const lastNavButton = carousel.querySelector('div > div:last-child p'); // Last button (Previous)
 
-    let  pathname = null;
-    if(val){
-        pathname=jsonURL;
-    }else{
-        pathname= new URL(jsonURL);
-    }
-    
-    const resp = await fetch(pathname);
-    const json = await resp.json();
-    console.log("=====JSON=====> {} ",json);
-    
-    const table = document.createElement('table');
-    createTableHeader(table);
-    json.data.forEach((row,i) => {
+    // Dynamically add unique IDs to the Next and Previous buttons
+firstNavButton.id = "next-button";
+lastNavButton.id = "prev-button";
 
-        createTableRow(table,row,(i+1));
+    // Dynamically assign IDs to slides
 
-      
+    slides.forEach((slide, index) => {
+slide.id = "slide-" + (index + 1);
+
     });
-    
-    return table;
-}    
 
-export default async function decorate(block) {
-    const countries = block.querySelector('a[href$=".json"]');
-    const parientDiv=document.createElement('div');
-    parientDiv.classList.add('contries-block');
+    // Add event listeners to the navigation buttons
 
-    if (countries) {
-        parientDiv.append(await createSelectMap(countries.href));
-        parientDiv.append(await createTable(countries.href,null));
-        countries.replaceWith(parientDiv);
-        
-    }
-    const dropdown=document.getElementById('region');
-      dropdown.addEventListener('change', () => {
-        let url=countries.href;
-        if(dropdown.value!='all'){
-            url=countries.href+"?sheet="+dropdown.value;
+    firstNavButton.addEventListener('click', goToNextSlide);
+
+    lastNavButton.addEventListener('click', goToPreviousSlide);
+
+    // Track the current slide index
+
+    let currentSlideIndex = 0;
+
+    // Function to go to the next slide
+
+    function goToNextSlide() {
+
+        if (currentSlideIndex < slides.length - 1) {
+
+            currentSlideIndex++;
+
+        } else {
+
+            currentSlideIndex = 0; // Loop back to the first slide
+
         }
-        const tableE=parientDiv.querySelector(":scope > table");
-        let promise = Promise.resolve(createTable(url,dropdown.value));
-        promise.then(function (val) {
-            tableE.replaceWith(val);
+
+        updateCarousel();
+
+    }
+
+    // Function to go to the previous slide
+
+    function goToPreviousSlide() {
+
+        if (currentSlideIndex > 0) {
+
+            currentSlideIndex--;
+
+        } else {
+
+            currentSlideIndex = slides.length - 1; // Loop back to the last slide
+
+        }
+
+        updateCarousel();
+
+    }
+
+    // Function to update the carousel view and show the current slide
+
+    function updateCarousel() {
+
+        slides.forEach((slide, index) => {
+
+            if (index === currentSlideIndex) {
+
+                slide.style.display = "block"; // Show the current slide
+
+            } else {
+
+                slide.style.display = "none"; // Hide all other slides
+
+            }
+
         });
-      });
-  }
+
+    }
+
+    // Initialize the carousel by showing the first slide
+
+    updateCarousel();
+
+});
+ console.log("HELLO")
